1ee072c68bc4a7930fc49a32ec08826285c79461
Charlie Rosander <charile.plugga@gmail.com>
2022-09-02 14:19:55 +0200

    snyggade till koden

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 namn = input("Hej, vad heter du?: ")
-print("Hej,",namn)
+print("Hej,", namn)
 
 age1 = int(input('Hur gammal är du?: '))
 age2 = (age1+5)
-print('Okej, du är',age1)
-print('Om 5 år är du då',age2)
+print('Okej, du är', age1)
+print('Om 5 år är du då', age2)
